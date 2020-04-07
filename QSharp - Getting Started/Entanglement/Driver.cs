5c1782712ccf3dd70184f84bf8a725b43381c3fd
Edward <ewalbach@gmail.com>
2020-04-07 11:53:18 +0300

    initial

---
+++ QSharp - Getting Started/Entanglement/Driver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Quantum.Entanglement
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            var ones = 0;
+            var equal = 0;
+            using (var qsim = new QuantumSimulator())
+            {
+                for (int index = 0; index < 1000; index++)
+                {
+                    var (qubitOneState, qubitTwoState) = Entanglement.Run(qsim).Result;
+                    if (qubitOneState == Result.One)
+                    {
+                        ones++;
+                    }
+
+                    if (qubitOneState == qubitTwoState)
+                    {
+                        equal++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Entanglement Results: ");
+            Console.WriteLine($"\t   One: {ones}");
+            Console.WriteLine($"\t  Zero: {1000-ones}");
+            Console.WriteLine($"\t Equal: {equal/1000*100}%");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ QSharp - Getting Started/Entanglement/Entanglement.qs
@@ -0,0 +1,22 @@
+﻿namespace Quantum.Entanglement
+{
+    open Microsoft.Quantum.Intrinsic;
+    open Microsoft.Quantum.Canon;
+    
+    operation Entanglement () : (Result, Result) {
+        mutable qubitState01 = Zero;
+        mutable qubitState02 = Zero;
+        using ((qubit01, qubit02) = (Qubit(), Qubit())) {
+            H(qubit01);
+            CNOT(qubit01, qubit02);
+
+            set qubitState01 = M(qubit01);
+            set qubitState02 = M(qubit02);
+
+            Reset(qubit01);
+            Reset(qubit02);
+		}
+
+        return (qubitState01, qubitState02);
+    }
+}
